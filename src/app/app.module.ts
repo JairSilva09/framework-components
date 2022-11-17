23ac67ab8444fa2c055fc4361a8006d89160a161
Jair Silva <harryhaller30@gmail.com>
2022-11-17 14:10:39 -0500

    delete components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoxesComponent } from './boxes/boxes.component';
-import { ItemComponent } from './item/item.component';
 import { FormsModule } from '@angular/forms';
-import { StoreComponent } from './store/store.component';
-import { ItemCardComponent } from './item-card/item-card.component';
 import { CrudeComponentComponent } from './crude-component/crude-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -19,10 +14,6 @@ import { PaginationComponent } from './pagination/pagination.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BoxesComponent,
-    ItemComponent,
-    StoreComponent,
-    ItemCardComponent,
     CrudeComponentComponent,
     PaginationComponent
   ],
