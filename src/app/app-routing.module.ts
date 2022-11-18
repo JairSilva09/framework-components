2b9780d03ba8e2aee40b0dec5ca0da32e10d6694
Jair Silva <harryhaller30@gmail.com>
2022-11-18 10:09:33 -0500

    delete component crude

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CrudeComponentComponent } from './crude-component/crude-component.component';
 import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
-  {path: '**',component: CrudeComponentComponent},
-  // {path: 'store',component: StoreComponent},
-  { path: '**', pathMatch: 'full', redirectTo: '**' },
+  {path: 'store',component: StoreComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'store' },
 ];
 
 @NgModule({
