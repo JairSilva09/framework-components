d7194b641530fc9f15012f56f1ec46d7f94abd95
Jair Silva <harryhaller30@gmail.com>
2022-11-18 23:49:18 -0500

    created service data

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBoxComponent implements OnInit {
 
+  @ViewChild('searchBar') searchBar!: ElementRef;
+
+  @Output() search = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  ngAfterViewInit(){
+
+    fromEvent(this.searchBar.nativeElement, 'keyup')
+      .pipe(
+        filter(Boolean),
+        debounceTime(800),
+        distinctUntilChanged(),
+        tap((text: any) => {
+
+          let search = this.searchBar.nativeElement.value;
+         
+          console.log(search)
+
+          this.search.emit(search)
+
+            //this.getWirelessBySearch(this.search)
+
+        })
+      )
+    .subscribe();
+
   }
 
 }
