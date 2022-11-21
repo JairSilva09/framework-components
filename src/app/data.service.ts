28f5ce8b8ad0ad9e28b320c24cd8d3f96776164c
Jair Silva <harryhaller30@gmail.com>
2022-11-21 12:22:31 -0500

    add import excell

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { DATA } from './mock-data';
+import { map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,23 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   constructor() { }
+
+  getDirectory(): Observable<any[]>{
+    const directory =  of(DATA)
+    return directory;
+  }
+
+  getDirectoryBySearch(search: any): Observable<any[]>{ 
+    
+    const result = DATA.filter(a => a.category === search || a.client === search)
+    return  of(result)
+  }
+
+  getDirectorySort(sort: any): Observable<any[]>{
+    console.log(sort)
+    //this.carros.sort(((a, b) => a.modelo - b.modelo));
+    const result = DATA.sort(((a, b) => a.sort - b.sort))
+    return  of(result)
+    
+  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-table',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  data: any= [];
+  dataSource: any[] = [];
+  imported: any;  
+  observableSubs: any;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.getDirectory();
+  }
+
+  ngOnDestroy(){ if (this.observableSubs) this.observableSubs.unsubscribe(); }
+
+  getDirectory(): void{
+    this.observableSubs = this.dataService.getDirectory().subscribe(    
+      data => this.data = data,
+      error => console.log(error),
+    )  
+    this.dataSource = this.data
+  }
+
+  searchBy(search: string){
+    this.dataService.getDirectoryBySearch(search).subscribe(
+      data => this.data = data,
+      error => console.log(error),
+    )
+    this.dataSource = this.data    
+  }
+
+  importedData(data: any){   
+    
+    if(data != "" && data != undefined){
+
+      let text = "";
+
+      data.forEach((e:any) => {
+        
+        for (const key in e) {
+          if (Object.prototype.hasOwnProperty.call(e, key)) {
+            text += e[key]+" ";
+          }
+        }
+
+        text += "\n"
+
+      });
+
+      this.imported = text; 
+       
+    }else{
+      this.imported = "";
+    }
+       
   }
 
 }
